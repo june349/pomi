02e4ec9c197b3a1407318d5bef3a7482f2dfca34
june <176018493+june349@users.noreply.github.com>
2024-12-19 22:37:39 -0500

    validate time values are >0

--- src/cli.rs
+++ src/cli.rs
@@ -1,19 +1,19 @@
-use clap::{Parser, ValueHint};
+use clap::{value_parser, Parser, ValueHint};
 use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(version, about)]
 pub struct Args {
     /// Work session duration
-    #[arg(short, long, default_value_t = 25, value_name = "MINS")]
+    #[arg(short, long, default_value_t = 25, value_name = "MINS", value_parser = value_parser!(u32).range(1..))]
     pub work: u32,
 
     /// Long break duration
-    #[arg(short, long, default_value_t = 15, value_name = "MINS")]
+    #[arg(short, long, default_value_t = 15, value_name = "MINS", value_parser = value_parser!(u32).range(1..))]
     pub long_break: u32,
 
     /// Short break duration
-    #[arg(short, long, default_value_t = 5, value_name = "MINS")]
+    #[arg(short, long, default_value_t = 5, value_name = "MINS", value_parser = value_parser!(u32).range(1..))]
     pub short_break: u32,
 
     /// Sound file to play when a session is finished
